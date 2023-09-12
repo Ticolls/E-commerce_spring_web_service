0f6d35a1dda2b9c578fa0c45cee685bcd1b8c5b7
Thiago <ticolls@hotmail.com>
2023-09-11 22:29:48 -0300

    Payment entity

--- src/main/java/com/ticolls/web_service/config/TestConfig.java
+++ src/main/java/com/ticolls/web_service/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ticolls.web_service.entities.Category;
 import com.ticolls.web_service.entities.Order;
 import com.ticolls.web_service.entities.OrderItem;
+import com.ticolls.web_service.entities.Payment;
 import com.ticolls.web_service.entities.Product;
 import com.ticolls.web_service.entities.User;
 import com.ticolls.web_service.enums.OrderStatus;
@@ -67,9 +68,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID ,u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o1 = new Order(null, Instant.parse("2023-08-20T19:53:07Z"), OrderStatus.PAID ,u1);
+        Order o2 = new Order(null, Instant.parse("2023-08-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 
         userRepository.saveAll(Arrays.asList(u1, u2));
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2023-08-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 
